62f84a38259140cd83af9b1ea81ade8b63d4418b
Jessica Aliberti <jessicaaliberti@Jessicas-MacBook-Air.local>
2024-02-15 13:12:57 -0800

    Testing var selections and if statements

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var cat = document.querySelector ("cat");
+var button = document.querySelector ("show-cat");
+
+if (cat.classList.contains("show")) {
+    console.log("Yes");
+}
+else {console.log("No");};
