0ee54d372f4cd20a1377baf26666336e1af17f0d
Jessica Aliberti <jmaliberti@gmail.com>
2024-02-23 13:00:18 -0800

    Fixed button event listener - all working now.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var cat = document.querySelector (".cat");
 var button = document.querySelector (".show-cat");
 
+button.addEventListener("click", function () {
 if (cat.classList.contains("show")) {
-    cat.classList.remove("show");
-
-}
+    cat.classList.remove("show");}
 else {cat.classList.add("show");};
+});
