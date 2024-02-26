48b263594fee5bd5f59a00fceb2547aa4db54878
Jessica Aliberti <jmaliberti@gmail.com>
2024-02-26 12:45:56 -0800

    Added classlist disappear clauses to if and else statements

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ var button = document.querySelector (".show-cat");
 button.addEventListener("click", function () {
 if (cat.classList.contains("show")) {
     cat.classList.remove("show");
+    button.classList.add("disappear");
     button.innerText = "Wait, come back!";}
 else {cat.classList.add("show");
+    button.classList.remove("disappear");
     button.innerText = "Shoo, cat!";};
 });
